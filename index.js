92f1d79dfb221f92f7ce8d425516e912acac72df
Kevin Boyle <papershack@gmail.com>
2019-04-15 11:47:17 -0700

    add assets, jQuery, hover effect to contacts

---
+++ index.js
@@ -0,0 +1,14 @@
+function contactLinkHover(){
+    console.log('yo');
+    $('.contact-link-container img').hover(function(){
+        $(this).parents('.contact-link-container').css('background-color', '#ff000078');
+    }, function(){
+        $(this).parents('.contact-link-container').css('background-color', 'transparent');
+    })
+}
+
+
+
+
+$(contactLinkHover())
+
